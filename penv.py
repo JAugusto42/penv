6679ca62a82279557bf9523d5d58878339b070d9
Jose Augusto <joseaugusto.881@outlook.com>
2019-11-15 08:10:42 -0300

    adding more code, but not functional yet keke

--- penv.py
+++ penv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
 from pathlib import Path
@@ -6,23 +6,37 @@ from pathlib import Path
 
 class Main():
     """
-    DOC
+    Main Class
     """
     def __init__(self):
         self.dirpath = os.getcwd()
         self.folder_name = os.path.basename(self.dirpath)
         self.command_to_create_venv = 'virtualenv ' + self.folder_name
+        # TODO refatoring
         self.home_dir = str(Path.home())
-        self.venv_dir = self.home_dir + "/.local/share/venvs/"
+        self.venv_dir = self.home_dir + "/.local/share/virtualenvs/"
         self.check_dir()
 
     def check_dir(self):
-        if os.path.exists(self.venv_dir):
-            # activate venv
-        else:
-            os.makedirs(self.venv_dir)
+        self.venv_name = self.venv_dir + self.folder_name + '_venv'
 
-            print('verify if exists requirements.txt')
+        if os.path.exists(self.venv_dir):
+            if os.path.exists(self.venv_name):
+                self.command_to_activate_venv = "source " + self.venv_name + "/bin/activate"
+                os.system(self.command_to_activate_venv)
+                # TODO verify if requirements is update
+
+            else:
+                response = input('This project dont have an virtualenvironment create? [Y/n]')
+                print(response)
+                print('creating a virtualenv')
+                os.system('virtualenv ' + self.venv_name)
+                print('verify if exists requirements.txt')
+        else:
+            print('Dir virtualenv on {} not exists, creating...'.format(self.venv_dir))
+            os.makedirs(self.venv_dir)
+            return check_dir()
+
 
 
 Main()
